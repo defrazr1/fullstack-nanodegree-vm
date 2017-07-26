64581b3931a76c3db0c6064ed9fd272e6678f211
Demitrio Frazier <dfdefrazr@gmail.com>
2017-07-26 05:17:44 -0700

    working with SQLAlchemy to create database

---
+++ vagrant/database_setup.py
@@ -0,0 +1,26 @@
+import sys
+
+from sqlalchemy import
+Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import
+declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+class menuItem(Base):
+    __tablename__ = 'menu_item'
+#####insert at end of file####
+
+engine = create_engine(
+'sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
