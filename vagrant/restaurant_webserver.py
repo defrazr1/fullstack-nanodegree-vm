131246f45f4de46dde3be0faf3f50dd9e935a58b
Demitrio Frazier <dfdefrazr@gmail.com>
2017-08-03 04:36:02 -0700

    updated webserver to add to database

--- vagrant/restaurant_webserver.py
+++ vagrant/restaurant_webserver.py
@@ -11,25 +11,40 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 class WebServerHandler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         try:
-            if self.path.endswith("/restaurants"):
+            if self.path.endswith("/restaurants/new"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output = ""
                 output += "<html><body>"
-                output += "<h1>Restaurants</h1>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/new'>"
+                output += "<input name='newRestaurantName' type='text' placeholder = 'New Restaurant Name'>"
+                output += "<input type='submit' value='Create'>"
+                output += "</form></body></html>"
+                self.wfile.write(output)
+                print(output)
+                return
 
+            if self.path.endswith("/restaurants"):
                 restaurants = session.query(Restaurant).all()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Restaurants</h1>"
+                output += "<a href = '/restaurants/new'> Make a New Restaurant Here </a></br></br>"
+
                 for restaurant in restaurants:
                     output += "<br><font size='5'>%s</font>" % (restaurant.name)
                     output += "<br><font size='3'><a href=>Edit</a></font>"
                     output += "<br><font size='3'><a href=>Delete</a></font><br>"
 
-
-                output += "</html><body>"
+                output += "</body></html>"
                 self.wfile.write(output)
                 print(output)
                 return
@@ -40,35 +55,32 @@ class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            output = ""
-            output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
         except:
             pass
-
-
 
 def main():
     try:
         port = 8080
         server = HTTPServer(('', port), WebServerHandler)
-        print "Web Server running on port %s" % port
+        print ("Web Server running on port %s" % (port))
         server.serve_forever()
     except KeyboardInterrupt:
-        print " ^C entered, stopping web server...."
+        print (" ^C entered, stopping web server....")
         server.socket.close()
 
 if __name__ == '__main__':
