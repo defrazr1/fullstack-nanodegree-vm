47037b516ebf6f1d47ec82e4fec3e3726e37d18c
Demitrio Frazier <dfdefrazr@gmail.com>
2017-08-05 18:25:43 -0700

    removed html coding within script and created templates for better use of Flask framework

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, Base, MenuItem
@@ -15,15 +15,7 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
-
-    output = ''
-    for item in items:
-        output += item.name
-        output += ('<br>%s' % (item.price))
-        output += ('<br>%s' % (item.description))
-        output += '<br><br>'
-
-    return output
+    return render_template('menu.html', restaurant = restaurant, items = items)
 
 @app.route('/restaurants/<int:restaurant_id>/new/')
 def newMenuItem(restaurant_id):
